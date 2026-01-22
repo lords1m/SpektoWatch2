ebf82b4ef598eaa5f93520285bd7027c5bcf50a3
lords1m <simeon.brandt@gmail.com>
2026-01-22 11:35:42 +0100

    Initial Commit

---
+++ SpektoWatch2/SpektoWatch2App.swift
@@ -0,0 +1,17 @@
+//
+//  SpektoWatch2App.swift
+//  SpektoWatch2
+//
+//  Created by Simeon Brandt on 22.01.26.
+//
+
+import SwiftUI
+
+@main
+struct SpektoWatch2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
