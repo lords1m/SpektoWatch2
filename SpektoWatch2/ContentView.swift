c726ecfeeab0936f07d1f80eda1883fb233f0251
lords1m <75284704+lords1m@users.noreply.github.com>
2026-01-27 14:33:30 +0100

    Fix: ContentView - entferne audioEngine Parameter da er intern erstellt wird

--- SpektoWatch2/ContentView.swift
+++ SpektoWatch2/ContentView.swift
@@ -1,9 +1,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var audioEngine = AudioEngine()
-    
     var body: some View {
-        ModularDashboardView(audioEngine: audioEngine)
+        ModularDashboardView()
     }
 }
