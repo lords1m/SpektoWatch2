1ce011f16acfa03340453901456efca723416a55
lords1m <75284704+lords1m@users.noreply.github.com>
2026-01-28 16:20:41 +0100

    Add gainOrBandwidthChangedNotification for Watch App

--- Shared/NotificationNames.swift
+++ Shared/NotificationNames.swift
@@ -3,4 +3,5 @@ import Foundation
 extension NSNotification.Name {
     static let startRecordingCommand = NSNotification.Name("startRecordingCommand")
     static let stopRecordingCommand = NSNotification.Name("stopRecordingCommand")
+    static let gainOrBandwidthChangedNotification = NSNotification.Name("gainOrBandwidthChangedNotification")
 }
