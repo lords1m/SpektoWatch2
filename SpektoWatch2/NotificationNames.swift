708563224d830d51625c48416b50335030532256
lords1m <75284704+lords1m@users.noreply.github.com>
2026-01-28 16:18:43 +0100

    Remove duplicate Logger+Extensions and NotificationNames from SpektoWatch2 folder (kept in Shared)

--- SpektoWatch2/NotificationNames.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-extension NSNotification.Name {
-    static let startRecordingCommand = NSNotification.Name("startRecordingCommand")
-    static let stopRecordingCommand = NSNotification.Name("stopRecordingCommand")
-}
