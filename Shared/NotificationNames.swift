9f379a7fcc5c77e543d2d1ae65b4da05c623f843
lords1m <75284704+lords1m@users.noreply.github.com>
2026-01-28 16:16:05 +0100

    Add shared Logger and Notification extensions for Watch App

---
+++ Shared/NotificationNames.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+extension NSNotification.Name {
+    static let startRecordingCommand = NSNotification.Name("startRecordingCommand")
+    static let stopRecordingCommand = NSNotification.Name("stopRecordingCommand")
+}
